51ecca2ab84e0927344866968615ae658fa713de
ThePastaNebula <65950050+ThePastaNebula@users.noreply.github.com>
2025-08-03 03:45:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ import routes from './routes.js';
 export const store = Vue.reactive({
     dark: JSON.parse(localStorage.getItem('dark')) || false,
     toggleDark() {
-        this.dark = !this.dark;
-        localStorage.setItem('dark', JSON.stringify(this.dark));
+        this.dark = true;
     },
 });
 
