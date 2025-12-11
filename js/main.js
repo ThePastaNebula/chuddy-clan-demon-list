3864913750fd4128b4d07a91af0ab0a8be0c230f
ThePastaNebula <65950050+ThePastaNebula@users.noreply.github.com>
2025-12-11 21:52:34 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 import routes from './routes.js';
 
 export const store = Vue.reactive({
-    dark: JSON.parse(localStorage.getItem('dark')) || false,
-    toggleDark() {
-        this.dark = true;
-    },
+    dark: true,
 });
 
 const app = Vue.createApp({
